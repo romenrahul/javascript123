0f63e6dc167078ee10649d00498c36f26fbb8568
romenrahul <romenrahul884@gmail.com>
2022-07-13 00:40:32 -0700

    second try

---
+++ app.js
@@ -0,0 +1,5 @@
+let btn = document.getElementById("bnt")
+addEventListener("click", ()=>{
+    document.getElementById("img").src="https://i.pinimg.com/736x/30/6c/cf/306ccf8e752bf35a72d0d8b44475e78e.jpg"
+    document.getElementById("img").alt="mujpr mat atko"
+})
